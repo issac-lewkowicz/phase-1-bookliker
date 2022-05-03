4b022b05ec689249df814b71607e00b35fa61493
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-05-03 17:31:05 -0600

    fixed continous appendance

--- js/index.js
+++ js/index.js
@@ -7,16 +7,18 @@ function getBooks(){
     fetch('http://localhost:3000/books')
     .then(stuff => stuff.json())
     // invoke renderbooks
-    .then(bookList => renderBooks(bookList));
+    .then(bookList => renderBooks(bookList))
   }
 
 function renderBooks(books) {
-    const list = document.querySelector('#list')
+    const list = document.querySelector('#list');
     books.forEach(book => {
-      const li = document.createElement('li')
-      li.innerHTML = book.title
-      list.appendChild(li)
-      li.addEventListener('click', () => infoGetter(book))
+      const li = document.createElement('li');      
+      li.addEventListener('click', () => infoGetter(book));
+      
+      li.textContent = book.title;
+      list.appendChild(li);
+      
     })
     
 }
@@ -26,12 +28,18 @@ function renderBooks(books) {
 
 
 function infoGetter (book) {
-    const info = document.querySelector('#show-panel')
-    const thumbnail = document.createElement('img')
-    const description = document.createElement('p')
-    description.innerText = book.description
-    thumbnail.src = book.img_url
-    info.appendChild(thumbnail)
-    info.appendChild(description)
-    console.log(info)
+    const info = document.querySelector('#show-panel');
+    info.innerHTML = "";
+    const thumbnail = document.createElement('img');
+    const title = document.createElement('h1');
+    const subtitle = document.createElement('h2');
+    const author = document.createElement('h3');
+    const description = document.createElement('p');
+    description.textContent = book.description;
+    thumbnail.src = book.img_url;
+    title.textContent = book.title;
+    subtitle.textContent = book.subtitle;
+    author.textContent = book.author;
+    info.append(thumbnail, title, subtitle, author, description );
+    console.log(info);
 }
